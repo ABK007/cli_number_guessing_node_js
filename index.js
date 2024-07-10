f115e911d1949a08e08e65a15139b574f162329f
Muhammad Abubakar Khalid <31004320+ABK007@users.noreply.github.com>
2024-07-10 11:03:15 +0500

    Added the random number generation logic from 1 to 10

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 import inquirer from "inquirer";
-let number = 12;
+// Generating random number
+const randomNumber = Math.floor(Math.random() * 10 + 1);
+console.log("Guess Number " + randomNumber);
 // Getting user input
 const answers = await inquirer.prompt([
     { name: "userGuessedNumber",
         type: "number",
-        message: "Please guess a number: "
+        message: "Please guess a number between from 1 to 10: "
     },
 ]);
 // Applying logic to check user input number
-if (answers.userGuessedNumber === number) {
+if (answers.userGuessedNumber === randomNumber) {
     console.log("You have guessed the correct number");
 }
 else {
--- index.ts
+++ index.ts
@@ -1,19 +1,25 @@
 import inquirer from "inquirer";
+import chalk from "chalk"
 
 
-let number = 12;
+
+// Generating random number
+const randomNumber = Math.floor(Math.random() * 10 + 1);
+
+console.log("Guess Number " + randomNumber )
+
 // Getting user input
 const answers = await inquirer.prompt([
     {name: "userGuessedNumber",
     type: "number",
-    message:"Please guess a number: "
+    message:"Please guess a number between from 1 to 10: "
 
     },
 
 ]);
 
 // Applying logic to check user input number
-if (answers.userGuessedNumber === number) {
+if (answers.userGuessedNumber === randomNumber) {
     console.log("You have guessed the correct number")
 } else {
     console.log("You have guessed the wrong number")
