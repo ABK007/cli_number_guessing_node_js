d04f2d5c424a0f689f050f9312d098bdda58b2c9
Muhammad Abubakar Khalid <31004320+ABK007@users.noreply.github.com>
2024-07-10 10:41:45 +0500

    Get user input and compare it with number

--- index.ts
+++ index.ts
@@ -1,3 +1,20 @@
 import inquirer from "inquirer";
 
-const answers = await inquirer.prompt([])
+
+let number = 12;
+// Getting user input
+const answers = await inquirer.prompt([
+    {name: "userGuessedNumber",
+    type: "number",
+    message:"Please guess a number: "
+
+    },
+
+]);
+
+// Applying logic to check user input number
+if (answers.userGuessedNumber === number) {
+    console.log("You have guessed the correct number")
+} else {
+    console.log("You have guessed the wrong number")
+}
